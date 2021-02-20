abadeb23920c1731136b186e262c9903ea2b3182
Peng Jiang <pengjiang030@gmail.com>
2021-02-20 00:46:23 -0600

    .

--- custom_sparse_ops.py
+++ custom_sparse_ops.py
@@ -12,15 +12,10 @@ class SparseDenseMM(torch.autograd.Function):
   @staticmethod
   def forward(ctx, mat1, mat2):
     global spmm_forward_time
-    #print('forward: ', mat1._indices())
-    ctx.save_for_backward(mat1)
     t1 = time.time()
-    #output = spmm_cpp.spmm(mat1, mat2) 
-    output = mat1.mm(mat2) 
-    #output = output.reshape(mat2.shape[1], mat1.shape[0]).transpose(0,1)
-    #print('forward1: ', output)
-    #print('forward2: ', mat1.mm(mat2))
-    #print(torch.norm(output - mat1.mm(mat2)))
+    #print('forward: ', mat1._indices())
+    ctx.save_for_backward(mat1, mat2)
+    output = mat1.mm(mat2)
     spmm_forward_time += time.time() - t1
     return output
 
@@ -28,8 +23,9 @@ class SparseDenseMM(torch.autograd.Function):
   def backward(ctx, grad_output):
     global spmm_backward_time
     t1 = time.time()
-    mat1, = ctx.saved_tensors
+    mat1, mat2 = ctx.saved_tensors
     mat1 = mat1.transpose(0, 1)
+    #print('backward: ', type(grad_output))
     grad_mat2 = mat1.mm(grad_output)
     spmm_backward_time += time.time() - t1
     return None, grad_mat2
@@ -37,6 +33,7 @@ class SparseDenseMM(torch.autograd.Function):
 
 
 spmm = SparseDenseMM.apply
+
 
 
 #a = torch.randn(2, 3).to_sparse().cuda().requires_grad_(True)
